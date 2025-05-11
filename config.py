4311b629c990bc7abdbdbd6e2f48c561d10e0da2
Sethtatad KIJKANJANARAT <seth.kij@gmail.com>
2025-05-11 23:55:58 +0700

    revert speed

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Configs:
     __CONFIGS = {
         'FPS': 60,
-        'SPEED': 6,
+        'SPEED': 4,
         'WIN_SIZE_W': 800,
         'WIN_SIZE_H': 600,
         'WHITE': (255, 255, 255),
